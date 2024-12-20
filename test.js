ed3ac94287d2860c72a9935ecb24da8a563c1ca6
albaroudi-abdelilah <albaroudiabdelilah77@gmail.com>
2024-12-20 10:31:58 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+document.getElementById('emailForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const popup = window.open('', 'emailWindow', 'width=400,height=400');
+
+  popup.document.write(`
+    <html>
+      <body>
+        <form action="${form.action}" method="${form.method}" target="_self">
+          <input type="hidden" name="recipient_email" value="${form.recipient_email.value}">
+          <input type="hidden" name="subject" value="${form.subject.value}">
+        </form>
+        <script>
+          document.forms[0].submit();
+        </script>
+      </body>
+    </html>
+  `);
+});
